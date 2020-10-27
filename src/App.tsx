9f8576a1fa30077f01494a506747d7df2254db4d
Carl Wood <cjwood17@gmail.com>
2020-10-27 17:47:50 +0000

    Grab all prem teams from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [allTeams, setTeams] = useState<null | {teams: Array<object>}>(null);
+  const url = 'http://api.football-data.org/v2/competitions/2021/teams'
+  // url = 'https://api.football-data.org/v2/teams/1044/'
+
+  useEffect(() => {
+    fetch(url, {
+      headers: { 'X-Auth-Token': '8d55f4af6fca4ae5b79bbe9c64c6204b' },
+    })
+    .then(
+      (response) => {
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+          return;
+        }
+
+        // Examine the text in the response
+        response.json().then((data) => {
+          setTeams({teams: data.teams});
+        });
+      }
+    )
+    .catch((err) => {
+      console.log('Fetch Error :-S', err);
+    });
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Team Picker
       </header>
+      <p>Choose a team</p>
+
+      <select>
+        {allTeams && 
+          allTeams.teams.map((team:any) => {
+            console.log(team)
+            return (
+              <option key={team.id} value={team.id}>
+                {team.name}
+              </option>
+            )
+          })
+        }
+      </select>
     </div>
   );
 }
