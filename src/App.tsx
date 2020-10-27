67f616c05f6543b073922b6961f348a1dbbceb74
Carl Wood <cjwood17@googlemail.com>
2020-10-27 18:48:50 +0000

    Basic team select and show squad functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import './App.css';
 
 function App() {
   const [allTeams, setTeams] = useState<null | {teams: Array<object>}>(null);
+  const [selectedSquad, setSelectedSquad] = useState<null | { squad: Array<object> }>(null);
   const [loading, setLoading] = useState(true);
   const url = 'http://api.football-data.org/v2/competitions/2021/teams'
-  // url = 'https://api.football-data.org/v2/teams/1044/'
 
   useEffect(() => {
     fetch(url, {
@@ -31,6 +31,34 @@ function App() {
     });
   }, [])
 
+  const handleChange = (event: any) => { // todo: event type
+    const selectedTeam = event.target.value
+
+    fetch(`https://api.football-data.org/v2/teams/${selectedTeam}/`, {
+      headers: { 'X-Auth-Token': '8d55f4af6fca4ae5b79bbe9c64c6204b' },
+    })
+    .then(
+      (response) => {
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+          return;
+        }
+
+        // Examine the text in the response
+        response.json().then((data) => {
+          setSelectedSquad({ squad: data.squad });
+          // setLoading(false)
+          console.log(data.squad);
+          console.log(selectedSquad)
+        });
+      }
+    )
+    .catch((err) => {
+      console.log('Fetch Error :-S', err);
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -40,10 +68,10 @@ function App() {
       { loading && 'loading teams...' }
       <p>Choose a team</p>
 
-      <select>
+      <select onChange={handleChange}>
+        <option value="">Choose a team</option>
         {allTeams && 
           allTeams.teams.map((team:any) => {
-            console.log(team)
             return (
               <option key={team.id} value={team.id}>
                 {team.name}
@@ -52,6 +80,18 @@ function App() {
           })
         }
       </select>
+
+      { selectedSquad && 
+        selectedSquad.squad.map((player: any) => {
+          return (
+            <p key={player.id}>
+              {player.position} 
+              {player.position && ' | ' }
+              {player.name}
+            </p>
+          )
+        })
+      }
     </div>
   );
 }
